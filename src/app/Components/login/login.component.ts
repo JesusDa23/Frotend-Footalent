93bd778e526ae4db92989193245b70b15d8b56f7
JesarRL <ljesar01@gmail.com>
2024-09-28 08:07:42 -0500

    subiendo logica login register

---
+++ src/app/Components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountsService } from '../../Services/accounts.service';
+import { Credentials, resLoginUser } from '../../Interfaces/credentials';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+
+  dni: string = ""
+  password: string = ""
+
+  constructor(private _accountsService: AccountsService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  botonLogIn() {
+    console.log("DNI:", this.dni);
+    console.log("Password:", this.password);
+
+    if (this.dni == "" || this.password == "") {
+      alert("Todos los campos son obligatorios")
+    }
+
+    let user: Credentials = {
+      dni: this.dni,
+      password: this.password
+    }
+
+    this._accountsService.logIn(user).subscribe((data) => {
+      let dataCast = data as resLoginUser
+      sessionStorage.setItem('token', dataCast.token);
+    });
+
+  }
+
+  // ToDo: Si quieren implementar el login por medio de cookies, deben de armar la cookie desde el backend para enviarla al frontend y guardarla
+  // Para realizar esto, se debe de usar una libreria de node llamada cookie-parser, esta es la encargada de armar la cookie con la siguiente estructura
+  // res.cookie('cookieName', 'cookieValue', { maxAge: 900000, httpOnly: true });
+  // ? cookieName: es el nombre de la cookie
+  // ? cookieValue: es el valor de la cookie
+  // ? {} : es un objeto que contiene las opciones o configuraciones de la cookie
+
+
+}
---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './Components/login/login.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { ProfileComponent } from './Components/user/profile/profile.component';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', title: "Login", component: LoginComponent },
+  { path: 'signup', title: "Sign Up", component: SignupComponent },
+  { path: 'my-profile', title: "Personal Profile", component: ProfileComponent, canActivate: [authGuard] }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
