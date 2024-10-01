e06fc02e0e4e775e5c38c00c3d15984e985d2eb1
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-10-01 15:41:38 -0500

    se suben cambios

--- src/app/Components/admin/conductores/conductores.component.ts
+++ src/app/Components/admin/conductores/conductores.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-conductores',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './conductores.component.html',
   styleUrl: './conductores.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { SignupComponent } from './Components/signup/signup.component';
 import { ProfileComponent } from './Components/user/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
 import { HomeComponent } from './Components/admin/home/home.component';
+import { ConductoresComponent } from './Components/admin/conductores/conductores.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', title: "Login", component: LoginComponent },
   { path: 'signup', title: "Sign Up", component: SignupComponent },
   { path: 'my-profile', title: "Personal Profile", component: ProfileComponent, canActivate: [authGuard] },
-  { path:'home', component: HomeComponent, canActivate: [authGuard]}
+  { path:'home', component: HomeComponent, canActivate: [authGuard]},
+  { path: 'conductores', component: ConductoresComponent, canActivate:[authGuard]}
 ];
 
 @NgModule({
