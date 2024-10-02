2971365f523c3bd892193a1475955d3f7a7c678b
JesarRL <ljesar01@gmail.com>
2024-10-01 23:41:04 -0500

    acabando diseño login con alertas

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Credentials, UserInfo } from '../../Interfaces/credentials';
 import { AccountsService } from '../../Services/accounts.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterModule],
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
@@ -18,6 +20,7 @@ export class SignupComponent implements OnInit {
   confirmPassword: string = "";
   name: string = "";
   dni: string = "";
+  termsAccepted: boolean = false;
 
   constructor(private _accountsService: AccountsService, private router: Router) { }
 
@@ -25,13 +28,53 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void { }
 
   botonSignUp() {
+
+
+    // Validar si los campos estan vacios
     if (this.email === "" || this.password === "" || this.confirmPassword === "") {
-      alert("Todos los campos son obligatorios");
+
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Todos los campos son obligatorios.",
+        showConfirmButton: false,
+        timer: 1600
+      });
       return;
     }
 
+    // Validar si las claves son iguales
     if (this.password !== this.confirmPassword) {
-      alert("Las contraseñas no coinciden");
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Las contraseñas no coinciden.",
+        showConfirmButton: false,
+        timer: 1500
+      });
+      return;
+    }
+
+    // Validar si el email es correcto
+    if (!/^[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,}$/.test(this.email)) {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "El email no es valido.",
+        showConfirmButton: false,
+        timer: 1600
+      });
+      return;
+    }
+
+    if (!this.termsAccepted) {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "Debes aceptar los términos y condiciones.",
+        showConfirmButton: false,
+        timer: 1600
+      });
       return;
     }
 
@@ -46,13 +89,29 @@ export class SignupComponent implements OnInit {
     this._accountsService.signUp(user).subscribe({
       next: data => {
         console.log("El usuario fue registrado con éxito");
-        console.log("data", data);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "El usuario fue registrado con éxito.",
+          showConfirmButton: false,
+          timer: 1500
+        });
 
-        this.router.navigate(['/login']);
+        setTimeout(() => {
+          this.router.navigate(['/login']);
+        }, 1700);
       },
       error: err => {
-        console.error("Error al registrar el usuario:", err);
-        alert("Hubo un error al registrar el usuario. Inténtalo de nuevo.");
+        console.error("Error al registrar el usuario:", err.error);
+
+        Swal.fire({
+          position: "top-end",
+          icon: "error",
+          title: `Hubo un error al registrar el usuario. Inténtalo de nuevo.\n \nDetalle de error: ${err.error.error}`,
+          showConfirmButton: false,
+          timer: 1600
+        });
+
       }
     });
   }
