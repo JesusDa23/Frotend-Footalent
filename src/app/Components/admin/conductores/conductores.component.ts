9eda126548c3ee797ca61f64b457bc2daf1f4e30
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-10-10 13:54:26 -0500

    despliegue

--- src/app/Components/admin/conductores/conductores.component.ts
+++ src/app/Components/admin/conductores/conductores.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
 
@@ -8,11 +8,14 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
 
 import { AgregarConductorComponent } from '../agregar-conductor/agregar-conductor.component';
+import { NgModel } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-conductores',
   standalone: true,
-  imports: [HeaderComponent, AgregarConductorComponent, CdkDropList, CdkDrag, RouterLink],
+  imports: [HeaderComponent, AgregarConductorComponent, CdkDropList, CdkDrag, RouterLink, NgIf],
   templateUrl: './conductores.component.html',
   styleUrl: './conductores.component.css'
 })
@@ -21,19 +24,43 @@ export class ConductoresComponent {
   accountsService = inject(AccountsService)
   retrievedUsers: any[] = [];
   httpClient = inject(HttpClient)
+  isLoading = false;
 
   constructor(private http: HttpClient) { }
   private requestHeaders: HttpHeaders = new HttpHeaders(); 
 
 
   retrieveUsers() {
+    this.isLoading = true
     this.accountsService.retrieveUsers().subscribe((res: any) => {
       if (res) {
         this.retrievedUsers = res.data
+        this.isLoading = false
       } else {
         console.log("No se obtuvieron usuarios");
       }
     })
+
+
+    // this.accountsService.retrieveUsers().subscribe({
+    //   next: (data) => {
+    //     this.retrieveUsers = data;
+    //     if (this.retrieveUsers.length === 0) {
+    //       Swal.fire('Sin datos', 'No se encontraron vehículos.', 'warning');
+    //     }
+    //     this.isLoading = false;
+    //   },
+    //   error: (err) => {
+    //     console.error(err);
+    //     Swal.fire({
+    //       title: 'Error!',
+    //       text: 'Hubo un problema al cargar los vehículos. Intentando de nuevo...',
+    //       icon: 'error',
+    //       confirmButtonText: 'Intentar de nuevo',
+    //       confirmButtonColor: '#0A135D'  // Cambia el color del botón de confirmación
+    //     });
+    //   }
+    // });
   }
 
   drop(event: CdkDragDrop<string[]>) {
--- src/app/Components/admin/flota/mantenimientos/mantenimientos.component.ts
+++ src/app/Components/admin/flota/mantenimientos/mantenimientos.component.ts
@@ -1,4 +1,4 @@
-import { NgClass, NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { ChangeDetectorRef, Component, NgModule } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { MantenimientoService } from '../../../../Services/mantenimiento.service';
@@ -9,7 +9,7 @@ import { FlotaService } from '../../../../Services/flota.service';
 @Component({
   selector: 'app-mantenimientos',
   standalone: true,
-  imports: [FormsModule, NgFor, TogglemenuComponent, RouterModule],
+  imports: [FormsModule, NgFor, TogglemenuComponent, RouterModule, NgIf],
   templateUrl: './mantenimientos.component.html',
   styleUrl: './mantenimientos.component.css'
 })
@@ -18,6 +18,7 @@ export class MantenimientosComponent {
   vehicleId: any = ''
   flotas:any = [] 
   mantenimientoSelect: any[] = []
+  isLoading = false;
 
   constructor(
     private mantenimientoService: MantenimientoService,
@@ -27,33 +28,41 @@ export class MantenimientosComponent {
   ) { }
 
   ngOnInit() {
-    // Al iniciar, obtenemos los mantenimientos desde el servidor
+    this.isLoading = true;  // Activar el spinner al iniciar la carga
     this.vehicleId = this.route.snapshot.paramMap.get('vehicleId');
-    console.log(this.vehicleId)
+    console.log(this.vehicleId);
     this.obtenerMantenimientos();
-    this.loadFlotas()
+    this.loadFlotas();
   }
-
+  
   obtenerMantenimientos() {
     this.mantenimientoService.getMantenimientoById(this.vehicleId).subscribe(
       (data: any) => {
         this.mantenimientos = data.map((mantenimiento: any) => {
           // Formatear la fecha para que se ajuste al campo input date
           mantenimiento.fecha = new Date(mantenimiento.fecha).toISOString().split('T')[0];
-          return mantenimiento;  // Asegúrate de que el backend devuelve un array de mantenimientos
+          return mantenimiento;  
         });
-
+        this.isLoading = false;  // Desactivar el spinner cuando termine la carga
       },
       (error) => {
         console.error('Error al obtener mantenimientos:', error);
+        this.isLoading = false;  // Desactivar el spinner si ocurre un error
       }
     );
   }
-
+  
   loadFlotas() {
-    this.flotaService.getFlotas().subscribe(data => {
-      this.flotas = data;
-    });
+    this.flotaService.getFlotas().subscribe(
+      (data: any) => {
+        this.flotas = data;
+        this.isLoading = false;  // Desactivar el spinner cuando termine la carga
+      },
+      (error) => {
+        console.error('Error al cargar flotas:', error);
+        this.isLoading = false;  // Desactivar el spinner si ocurre un error
+      }
+    );
   }
 
   agregarMantenimiento() {
