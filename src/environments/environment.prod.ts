55f6a9d7cc6fccf3224633b5890e592552d0defd
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-10-15 08:38:05 -0500

    ruta environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/api/v1', // Adjust the production API URL as needed
+    apiUrl: 'https://backend-footalent.onrender.com/api/v1', // Adjust the production API URL as needed
   };
+  
