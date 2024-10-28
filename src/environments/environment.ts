e7f60684ca65b5f260511e508559e6c23aed8f9d
Jesús David Chavarro Castro <jesusdavidchavarro23@gmail.com>
2024-10-28 17:52:36 -0500

    se suvben cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
 
     production: false,
     apiUrl: 'https://backend-footalent-09ru.onrender.com/api/v1', // Adjust the API URL as needed
-    // Add other development environment variables here
+    // Add other development environment variables here    
 };
