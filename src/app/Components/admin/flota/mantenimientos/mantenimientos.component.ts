4cf385c1d7d4bd7a7b4d6d5a412df96f0d456a6c
JesarRL <ljesar01@gmail.com>
2024-10-31 19:47:39 -0500

    En zona de mantenimientos añadir filtro de fechas futuras ,tambien  filtro para unicamente aceptar numeros en el campo costo

--- src/app/Components/admin/flota/mantenimientos/mantenimientos.component.ts
+++ src/app/Components/admin/flota/mantenimientos/mantenimientos.component.ts
@@ -108,6 +108,15 @@ export class MantenimientosComponent {
     );
   }
 
+  getCurrentDate(): string {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // Enero es 0
+    const yyyy = today.getFullYear();
+    return `${yyyy}-${mm}-${dd}`;
+  }
+
+
   eliminarMantenimiento(id: number) {
     Swal.fire({
       title: '¿Estás seguro?',
